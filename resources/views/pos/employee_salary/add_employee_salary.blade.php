1090ff64f2a34395fc6c9d98bf5f5ea1ad292845
NayemHassan <nayem15-12498@diu.edu.bd>
2024-06-25 17:50:07 +0600

    Transaction Table  Update

--- resources/views/pos/employee_salary/add_employee_salary.blade.php
+++ resources/views/pos/employee_salary/add_employee_salary.blade.php
@@ -53,10 +53,22 @@
                             <span id="advancedSalary">Note: Avanced Amount:0</span>
 						</div>
 					</div><!-- Col -->
-					<div class="col-sm-12 form-valid-groups">
+					<div class="col-sm-6 form-valid-groups">
 						<div class="mb-3">
-							<label class="form-label">Note</label>
+							<label class="form-label">Select Bank Acoount <span class="text-danger">*</span></label>
+                            <select class="form-select js-example-basic-single" name="payment_method">
+                                 <option selected="" disabled>Select Bank Name</option>
+                                 @foreach ($bank as $item)
+                                <option value="{{$item->id}}">{{$item->name}}</option>
+                                @endforeach
+                            </select>
+						</div>
+					</div><!-- Col -->
+					<div class="col-sm-6 form-valid-groups">
+						<div class="mb-3">
+                            <label class="form-label">Note</label>
 							<textarea name="note" class="form-control" id=""  cols="20" rows="5"></textarea>
+
 						</div>
 					</div><!-- Col -->
 				</div><!-- Row -->
@@ -84,6 +96,9 @@
                 debit: {
                     required : true,
                 },
+                payment_method: {
+                    required : true,
+                },
             },
             messages :{
                 branch_id: {
@@ -97,6 +112,9 @@
                 },
                 debit: {
                     required : 'Please Enter Salary Amount',
+                },
+                payment_method: {
+                    required : 'Please Select Payment Method',
                 },
             },
             errorElement : 'span',
