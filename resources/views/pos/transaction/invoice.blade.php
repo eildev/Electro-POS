f3a11016d41e310e9a9306d58606cda3bb663267
NayemHassan <nayem15-12498@diu.edu.bd>
2024-07-03 10:43:50 +0600

    commit

--- resources/views/pos/transaction/invoice.blade.php
+++ resources/views/pos/transaction/invoice.blade.php
@@ -112,8 +112,6 @@
                             </td>
                         </tr>
                     </tbody>
-
-
                 </table>
         </div>
         <button class="btn btn-secondary btn-block" onclick="window.print();">
