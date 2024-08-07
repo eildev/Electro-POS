de652a69aea2eb6e0578d56eb642c70f1d6bdaa1
NayemHassan <nayem15-12498@diu.edu.bd>
2024-08-07 18:02:38 +0600

    multibranch update

--- resources/views/pos/damage/edit.blade.php
+++ resources/views/pos/damage/edit.blade.php
@@ -26,13 +26,12 @@
                                         <option selected disabled>Select Damaged Product</option>
                                         @foreach ($products as $product)
                                             <option value="{{ $product->id }}" {{$damage_info->product_id == $product->id ? 'selected' : ''}}> {{ $product->name.' ' .$product->unit->name}}</option>
-
                                         @endforeach
                                     @else
                                         <option selected disabled>Please Add Product</option>
                                     @endif
                                 </select>
-                                {{-- <input type="text" class="form-control" value="{{$damage_info->product->name}}" readonly> --}}
+
                                 <span class="text-danger product_select_error"></span>
                             </div>
                             <div class="col-sm-6">
