18f97fc097dc37ff8c9def14fe2ca49c5479f96e
kishormahmud9 <topfazil999@gmail.com>
2024-09-08 15:40:21 +0600

    Inventory Report is ongoing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,6 +35,7 @@ class DatabaseSeeder extends Seeder
             ExpenseCategorySeeder::class,
             SubcategorySeed::class,
             BrandSeed::class,
+            ProductSeed::class,
 
         ]);
     }
